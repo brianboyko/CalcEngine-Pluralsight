1b0b5fccc50a116014c96353c2a1e899ee898fcf
Brian Boyko <brian.boyko@gmail.com>
2016-07-10 00:47:47 -0500

    Added constructor initialization.

--- src/io/github/brianboyko/calcengine/MathEquation.java
+++ src/io/github/brianboyko/calcengine/MathEquation.java
@@ -5,10 +5,18 @@ package io.github.brianboyko.calcengine;
  */
 public class MathEquation {
 
-  public double leftVal;
-  public double rightVal;
-  public char opCode;
-  public double result;
+  private double leftVal;
+  private double rightVal;
+  private char opCode;
+  private double result;
+
+  public MathEquation(double leftVal, double rightVal, char opCode){
+    this.leftVal = leftVal;
+    this.rightVal = rightVal;
+    this.opCode = opCode;
+  }
+
+
 
   public void execute(){
     switch (opCode) {
@@ -31,5 +39,16 @@ public class MathEquation {
 
     }
   }
+
+  public double getResult(){
+    return this.result;
+  }
+
+  public double getLeftVal() {return leftVal;}
+  public double getRightVal() {return rightVal;}
+  public char getOpCode(){return opCode;}
+  public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+  public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+  public void setOpCode(char opCode) { this.opCode = opCode; }
 
 }
